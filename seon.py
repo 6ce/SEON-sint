817998c973a9a08bd08e28fdea9ac24dbc1a5142
M <86327263+6ce@users.noreply.github.com>
2024-09-17 20:49:41 -0700

    Update seon.py

--- seon.py
+++ seon.py
@@ -12,14 +12,23 @@ def _loadSessionFromConfig() -> dict:
         }
 
 class SEON:
+    """
+    A module for interacting with https://seon.io
+    """
     def __init__(self):
         self._cookies = _loadSessionFromConfig()
         self._refreshJWT()
 
     def _getRegisteredSocials(self, socials: dict) -> list[str]:
+        """
+        Returns a list of registered sites based on the input socials data
+        """
         return [site for site, data in socials.items() if data.get("registered") == True]
 
-    def _refreshJWT(self):
+    def _refreshJWT(self) -> str:
+        """
+        Refreshes the current JWT auth token
+        """
         try:
             url = "https://login.seon.io/api/auth/renew"
             data = {}
@@ -34,7 +43,10 @@ class SEON:
             print("Invalid session was passed")
             exit()
     
-    def _search(self, url: str, data: dict):
+    def _search(self, url: str, data: dict) -> list[str]:
+        """
+        Performs a SEON search on the input URL with the input data
+        """
         try:
             response = requests.post(url, cookies=self._cookies, json=data)
             details = response.json()["socialDetails"]
@@ -44,13 +56,19 @@ class SEON:
             self._refreshJWT()
             return self._search(url, data)
 
-    def email(self, email: str):
+    def email(self, email: str) -> list[str]:
+        """
+        Performs a SEON email search with the input email address
+        """
         print(f"Searching for email: {email}")
         url = "https://admin.seon.io/api/v2/manual/manual-input/email"
         data = {"email": email}
         return self._search(url, data)
     
     def phone(self, phone: str) -> list[str]:
+        """
+        Performs a SEON phone search with the input phone number
+        """
         print(f"Searching for phone: {phone}")
         url = "https://admin.seon.io/api/v2/manual/manual-input/phone"
         data = {"phone_number": phone}
